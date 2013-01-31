beb884dd50aed7de9779ab4602bfad63f207c91c
Josh Miklos <Josh.Miklos@gmail.com>
2013-01-31 16:01:32 -0700

    added sql stuff

---
+++ food.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE food;
+USE food;
+
+CREATE TABLE groceries{
+	name_id	INT UNSIGNED		NOT NULL	AUTO_INCREMENT,
+	price_per_ounce	FLOAT(4,4)	NOT NULL	DEFAULT	0,
+	price_per_pound	FLOAT(4,4)	NOT NULL	DEFAULT	0,
+	PRIMARY KEY	(name_id)
+} ENGINE=InnoDB;
+
+CREATE TABLE names{
+	name	VARCHAR(100)
+	common	BOOL/*TINYINT(1)*/	NOT NULL	DEFAULT=0,
+	name_id	INT UNSIGNED		NOT NULL,
+	FOREIGN KEY	(name_id) REFERENCES groceries(name_id)
+} ENGINE=InnoDB;
+
+CREATE TABLE type{
+	type_id INT UNSIGNED		NOT NULL AUTO_INCREMENT,
+	type_name	VARCHAR(100)	NOT NULL,
+	PRIMARY KEY	(type_id)
+} ENGINE=InnoDB;
+
+CREATE TABLE food_type_bridge{
+	food_id	INT UNSIGNED		NOT NULL,
+	type_id	INT UNSIGNED		NOT NULL,
+	FOREIGN KEY	(food_id) REFERENCES groceries(name_id),
+	FOREIGN KEY (type_id) REFERENCES type(type_id)
+} ENGINE=InnoDB;
